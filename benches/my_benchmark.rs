6fa58674a0b915b2e05f953eefed2fac9156e8d4
Adam Chalmers <adam.s.chalmers@gmail.com>
2024-12-05 14:21:39 -0600

    Fix codspeed

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -3,10 +3,10 @@ use aoc24_codspeed::day5::{part1, part2};
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
 pub fn criterion_benchmark(c: &mut Criterion) {
-    let input = include_str!("../input/2024/day5.txt");
+    let input = std::fs::read_to_string("input/2024/day5.txt").unwrap();
     c.bench_function("Run parts", |b| {
         b.iter(|| {
-            let parsed = input_generator(input);
+            let parsed = input_generator(&input);
             black_box(part1(&parsed));
             black_box(part2(&parsed));
         })
